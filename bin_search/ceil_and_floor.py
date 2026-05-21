ecd3b132e18d611fbbea97a26ac3859ac3275bdf
N.Vishnu Kumar <70788422+vishnuKumar3@users.noreply.github.com>
2026-05-21 18:15:12 +0530

    Update ceil_and_floor.py

--- bin_search/ceil_and_floor.py
+++ bin_search/ceil_and_floor.py
@@ -2,25 +2,31 @@
 https://takeuforward.org/arrays/floor-and-ceil-in-sorted-array
 '''
 def fun(nums, x):
-  n=len(nums)
-  s,e=0,n-1
-  floor,ceil=-1,-1 #if there is no ceil and floor, then we are returning -1 as the value
-  while(s<=e):
-    mid=(s+e)//2
-    if(nums[mid]==x):
-      ceil,floor=nums[mid],nums[mid]
-      break
-    elif(nums[mid]>x):
-      ceil=nums[mid]
-      e=mid-1
-    else:
-      floor=nums[mid]
-      s=mid+1
-  print(ceil,floor)
+    n=len(nums)
+    s,e=0,n-1
+    floor,ceil=-1,-1 #if there is no ceil and floor, then we are returning -1 as the value
+    while(s<=e):
+        mid=(s+e)//2
+        if(nums[mid]>x):
+            ceil=nums[mid]
+            e=mid-1
+        else:
+            s=mid+1
+    
+    s,e=0,n-1
+    while(s<=e):
+        mid=(s+e)//2
+        if(nums[mid]<x):
+            floor=nums[mid]
+            s=mid+1
+        else:
+            e=mid-1    
+    
+    print(ceil,floor)
   
 
-fun([3,4,4,7,8,10],5)
-fun([3,4,4,7,8,10], 8)
+fun([3,4,4,7,8,8,10],5)
+fun([3,4,4,7,8,8,10], 8)
 fun([1,2,3,4],5)
 
 '''
