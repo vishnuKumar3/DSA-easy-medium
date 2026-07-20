ea5ecaf73dd320750eddbe88d8f0c5dad4a331e1
N.Vishnu Kumar <70788422+vishnuKumar3@users.noreply.github.com>
2026-07-20 19:03:16 +0530

    Create painters_partition.py

---
+++ pending/painters_partition.py
@@ -0,0 +1,37 @@
+'''
+https://takeuforward.org/plus/dsa/problems/painters-partition?source=strivers-a2z-dsa-track
+'''
+class Solution:
+    def paint(self, A: int, B: int, C: list[int]) -> int:
+        n=len(C)
+        def isPossible(num):
+            count=0
+            s=0
+            i=0
+            while(i<n):
+                val=C[i]
+                s+=val
+                if(s>=num):
+                    if(s==num):
+                        i+=1
+                    count+=1
+                else:
+                    i+=1
+            if(s!=0):
+                if(s<=num):
+                    count+=1
+                else:
+                    count+=2
+            return count<=A
+        
+        s,e=max(C),sum(C)
+        res=0
+        while(s<=e):
+            mid=(s+e)//2
+            if(isPossible(mid)):
+                res=mid
+                e=mid-1
+            else:
+                s=mid+1
+        return res*B
+
